10d53d19528a204f739c8dc0868807dcb046dbe4
Inseo Song <41552919+fortunetiger@users.noreply.github.com>
2021-02-09 17:06:26 +0900

    Create 02_reverse_string.py

---
+++ ch02-6/02_reverse_string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        
+        s = s.reverse()
+        
+## Runtime: 160 ms, faster than 89.56% of Python online submissions for Reverse String.
+## Memory Usage: 21.3 MB, less than 31.00% of Python online submissions for Reverse String.
