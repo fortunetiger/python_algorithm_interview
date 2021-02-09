5019c09927ba54b9cf1b8ddfa0115a9437b221a2
Inseo Song <41552919+fortunetiger@users.noreply.github.com>
2021-02-09 18:08:29 +0900

    Create 04_most_common_word.py

---
+++ ch02-6/04_most_common_word.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        
+        words = re.split('[!?\',;. ]', str(paragraph).lower())
+                       
+        dic = set(words)
+        
+        print(dic)
+                       
+                       
+        maximum = 0
+        answer = ""
+        for word in dic:
+            if word == '' or (word in banned) : continue
+            else :
+                if  words.count(word) > maximum :
+                    maximum = words.count(word)
+                    answer = word
+        
+        return answer
+
+## Runtime: 20 ms, faster than 93.01% of Python online submissions for Most Common Word.
+## Memory Usage: 13.6 MB, less than 68.80% of Python online submissions for Most Common Word.
